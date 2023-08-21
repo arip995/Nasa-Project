d1812af2a5202e61937ce6d12eaeb822a7101643
arip995 <arindampanda2002@gmail.com>
2023-08-21 08:08:11 +0530

    added deployment setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path  = require('path')
 var cors = require('cors');
 
 const planetsRouter= require('./routes/planets/planets.router');
@@ -6,6 +7,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')))
 app.use('/planets',planetsRouter);
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
 
 module.exports = app;
