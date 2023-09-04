9a0ada79c78d58d23553b888e6b42d7b04d3d593
arip995 <arindampanda2002@gmail.com>
2023-09-04 08:18:44 +0530

    called a n api

--- server/src/app.js
+++ server/src/app.js
@@ -7,12 +7,16 @@ const planetsRouter= require('./routes/planets/planets.router');
 const app = express();
 
 app.use(cors());
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public')))
 app.use('/planets',planetsRouter);
-app.get('/*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','index.html'))
+// app.get('/*', (req,res)=>{
+//     res.sendFile(path.join(__dirname,'..','public','index.html'))
+// });
+
+app.get('/call',(req,res)=>{
+    res.status(200).json({name: "Panda"});
 })
 
 module.exports = app;
